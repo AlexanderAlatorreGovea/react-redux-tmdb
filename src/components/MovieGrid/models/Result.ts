51d24aeef9b34693373db7bd7d92eac10942b96d
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-16 20:35:57 -0700

    added typechecker service

--- src/components/MovieGrid/models/Result.ts
+++ src/components/MovieGrid/models/Result.ts
@@ -1,5 +1,4 @@
 import { Dates, MoviesData, MoviesResults } from "../types/Movie";
-import { typeChecker } from "../../../utils/typeChecker";
 import { Movie } from "./Movie";
 import { TypeChecker } from "../../../utils/_TypeChecker";
 
@@ -9,24 +8,14 @@ export class Result extends TypeChecker {
   dates: Dates | any;
   total_pages: number;
   total_results: number;
-  _getDates: (val: any) => any;
-  _getResults: (val1: string, val2: any, val3: string, val4: any) => any | [];
 
   constructor(data: MoviesData | any = {}) {
     super();
-
-    this._getDates = typeChecker("object", {}, "dates");
-    //this._getResults = typeChecker("array", [], "results");
-    this._getResults = this.checkValue("array", [], "results", data.results);
-
     this.page = data?.page || 0;
-    // this.results = this._getResults(data.results).map(
-    //   (movie: MoviesResults) => new Movie(movie)
-    // );
     this.results = this.checkValue("array", [], "results", data.results).map(
       (movie: MoviesResults) => new Movie(movie)
     );
-    this.dates = this._getDates(data.dates);
+    this.dates = this.checkValue("array", [], "results", data.dates);
     this.total_pages = data?.total_pages || 0;
     this.total_results = data?.total_results || 0;
   }
