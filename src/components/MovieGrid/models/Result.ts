a31272ba2312a4561c72d23fec75eca2d9d8d6e4
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-16 20:31:47 -0700

    saved this branch to save the changes without the typechecker class

--- src/components/MovieGrid/models/Result.ts
+++ src/components/MovieGrid/models/Result.ts
@@ -1,22 +1,29 @@
 import { Dates, MoviesData, MoviesResults } from "../types/Movie";
 import { typeChecker } from "../../../utils/typeChecker";
 import { Movie } from "./Movie";
+import { TypeChecker } from "../../../utils/_TypeChecker";
 
-export class Result {
+export class Result extends TypeChecker {
   page: number;
   results: MoviesResults[];
   dates: Dates | any;
   total_pages: number;
   total_results: number;
-  _getDates: (val: unknown) => any;
-  _getResults: (val: unknown) => any;
+  _getDates: (val: any) => any;
+  _getResults: (val1: string, val2: any, val3: string, val4: any) => any | [];
 
   constructor(data: MoviesData | any = {}) {
+    super();
+
     this._getDates = typeChecker("object", {}, "dates");
-    this._getResults = typeChecker("array", [], "results");
+    //this._getResults = typeChecker("array", [], "results");
+    this._getResults = this.checkValue("array", [], "results", data.results);
 
     this.page = data?.page || 0;
-    this.results = this._getResults(data.results).map(
+    // this.results = this._getResults(data.results).map(
+    //   (movie: MoviesResults) => new Movie(movie)
+    // );
+    this.results = this.checkValue("array", [], "results", data.results).map(
       (movie: MoviesResults) => new Movie(movie)
     );
     this.dates = this._getDates(data.dates);
---
+++ src/utils/_TypeChecker.ts
@@ -0,0 +1,40 @@
+export class TypeChecker {
+  public type: any;
+  public defaultValue: any;
+  public fieldName: any;
+  public value: any;
+
+  public checkValue(type: any, defaultValue: any, fieldName: any, value: any) {
+    if (type === "array" && !Array.isArray(value)) {
+      console.warn(`Expected ${fieldName} to be array but got ${typeof value}`);
+      return defaultValue;
+    }
+
+    if (type === "array" && Array.isArray(value)) return value;
+
+    if (type === "object" && Object.is(value, null)) {
+      console.warn(`Expected ${fieldName} to be object but got null`);
+      return defaultValue;
+    }
+
+    if (type === "object" && Array.isArray(value)) {
+      console.warn(`Expected ${fieldName} to be object but got array`);
+      return defaultValue;
+    }
+
+    if (typeof value !== type && Array.isArray(value)) {
+      console.warn(`Expected ${fieldName} to be ${type} but got array`);
+      return defaultValue;
+    }
+
+    if (typeof value !== type) {
+      console.warn(
+        `Expected ${fieldName} to be ${type} but got ${typeof this.value}`
+      );
+      return defaultValue;
+    }
+
+    return value;
+  }
+}
+
